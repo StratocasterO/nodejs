3e1b2bef152dda41a2852085bed8092108177825
StratocasterO <omarolmedoferrer@gmail.com>
2019-05-15 10:58:00 +0200

    formulario asistencia

--- amor/routes/db.js
+++ amor/routes/db.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-// añade ítems a la base de datos:
-var database=require('./database.js');
+var database = require('./database.js');
 
+// ESCALAS MUSICALES ===============================================================================================================
+// añade ítems a la base de datos:
 router.get('/enviar', function(req, res, next) {
 	const escala = req.query.escala;
 	const grado = req.query.grado;
@@ -21,6 +22,7 @@ router.get('/enviar', function(req, res, next) {
 	res.end(); 
 });
 
+// elimina ítems de la base de datos:
 router.get('/borrar', function(req, res, next) {
 	const id = req.query.id;
 
@@ -35,6 +37,7 @@ router.get('/borrar', function(req, res, next) {
 	res.end(); 
 });
 
+// modifica ítems de la base de datos:
 router.get('/modificar', function(req, res, next) {
 	const id = req.query.id;
 	const escala = req.query.escala;
@@ -53,13 +56,59 @@ router.get('/modificar', function(req, res, next) {
 	res.end(); 
 });
 
+// consulta la base de datos:
 router.get('/consultar', function(req, res, next) {
 	database.query('SELECT * FROM escalas', function(error,filas){
 		if(error){            
 			console.log('Se ha producido un error al escribir en la base de datos');
 			return;
 		};    
+		filas = JSON.stringify(filas);
+		res.writeHead(200);
+		res.write(filas);
+		res.end(); 
+	});
+	console.log("Se ha consultado una entrada de la base de datos");
+});
 
+// BASE DE DATOS ASISTENTES ==============================================================================================================
+// alta:
+router.get('/alta', function(req, res, next) {
+	const nombre = req.query.data;
+
+	database.query('INSERT INTO asistentes SET ?',{nombre:nombre}, function(error,filas){
+		if(error){            
+			console.log('Se ha producido un error al escribir en la base de datos');
+			return;
+		};    
+	});
+	console.log("Se ha añadido una entrada a la base de datos");
+	res.writeHead(200);
+	res.end(); 
+});
+
+// baja:
+router.get('/baja', function(req, res, next) {
+	const nombre = req.query.data;
+
+	database.query('DELETE FROM asistentes WHERE ?',{nombre:nombre}, function(error,filas){
+		if(error){            
+			console.log('Se ha producido un error al escribir en la base de datos');
+			return;
+		};    
+	});
+	console.log("Se ha añadido una entrada a la base de datos");
+	res.writeHead(200);
+	res.end(); 
+});
+
+// consulta:
+router.get('/consulta', function(req, res, next) {
+	database.query('SELECT * FROM asistentes', function(error,filas){
+		if(error){            
+			console.log('Se ha producido un error al escribir en la base de datos');
+			return;
+		};    
 		filas = JSON.stringify(filas);
 		res.writeHead(200);
 		res.write(filas);
